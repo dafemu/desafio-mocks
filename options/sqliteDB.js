a90aa90b328112cc334257b7d2c7b1d040f77d1f
David Munoz Barco <david.f.do@hotmail.com>
2022-09-07 18:37:32 -0500

    first commit

---
+++ options/sqliteDB.js
@@ -0,0 +1,11 @@
+const optionsSqlite = {
+  client: 'sqlite3',
+  connection: { 
+    filename: './db/desafio9.db3'
+  },
+  useNullAsDefault: true
+}
+
+module.exports = {
+  optionsSqlite
+}
---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+
+//Socket.io
+const { Server: HttpServer } = require('http');
+const { Server: IOServer } = require('socket.io');
+
+// //cargo el modulo handlebars
+const Handlebars = require("handlebars");
+
+//instancia
+const app = express();
+
+//requires propios
+const modBD = require('./insert_table');
+
+//socket.io
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
+//espacio publico del servidor
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//establecemos el motor de platilla que se utiliza
+app.set("view engine", "hbs");
+
+let productos = [];
+let mensajes = [];
+
+function setProducto(objProducto) {
+    console.log("entro al setproduct");
+    if(productos.length == 0){
+        objProducto.id = 1;
+    }else{
+        let id = productos[productos.length-1].id
+        objProducto.id = id + 1;
+    }
+    productos.push(objProducto);
+    return productos;
+}
+
+//socket.io etablecemos comunicacion
+io.on('connection', (socket) => {
+    console.log('un cliente se ha conectado');
+    socket.emit('productos', productos);
+    socket.emit('mensajes', mensajes);
+
+    socket.on('nuevo-producto', data => {
+        console.log('servidor productos');
+        console.log('data nuevo-producto: ', data);
+        setProducto(data)
+        io.sockets.emit('productos', productos);
+        modBD.insertProductMysql(data);
+    });
+
+    socket.on('nuevo-mensaje', data => {
+        console.log('servidor mensajes');
+        console.log('data nuevo-mensaje: ', data);
+        mensajes.push(data);
+        io.sockets.emit('mensajes', mensajes);
+        modBD.insertProductSqlite(data);
+    });
+});
+
+httpServer.listen(8080, () => {
+    console.log("Server on port 8080");
+});
